3cece82c575a13bea59226b1f3bf9c3524e42e16
Shane Fleming <shane.fleming06@imperial.ac.uk>
2015-08-04 16:19:30 +0000

    Original Schedule now stored in LostStateInjector class.

--- StitchUpBackend/LostStateInjector.h
+++ StitchUpBackend/LostStateInjector.h
@@ -8,16 +8,33 @@ static cl::opt<string>
 OriginalSchedule("orig_sched", cl::desc("Specify the schedule of the original LegUp circuit for StitchUp to reference")
 		 ,cl::value_desc("filename")); 
 
+struct BBstates {
+	unsigned stateCount;
+	std::string stateRef;
+};
+
+typedef std::vector<BBstates>::iterator BBState_iterator;
+
 namespace stchup {
  class LostStateInjector {
 	public:
 		LostStateInjector(FiniteStateMachine *fsm);	
+		~LostStateInjector();
 		void populateOrigBBStateNum();
+		BBState_iterator begin();
+		BBState_iterator end();
+		void print();
 	private:
-		std::vector<int> origBBStateNum;
+		std::vector<BBstates> origBBStateNum;
 		FiniteStateMachine *fsm;
 
  }; //namespace LostStateInjector
+
+	BBState_iterator LostStateInjector::begin()
+	{	return origBBStateNum.begin();		}
+
+	BBState_iterator LostStateInjector::end()
+	{	return origBBStateNum.end();		}
 
 	LostStateInjector::LostStateInjector(FiniteStateMachine *in)
 	{
@@ -25,22 +42,40 @@ namespace stchup {
 		populateOrigBBStateNum();
 	}
 
+	LostStateInjector::~LostStateInjector()
+	{
+		//print();
+	}	
+
 	void LostStateInjector::populateOrigBBStateNum()
 	{
 		std::ifstream origSchedule(OriginalSchedule);
 		if(!origSchedule.is_open()) { errs() << "Cannot open original schedule!\n"; assert(true);}	
 		std::string line;				
-		std::regex e ("Basic Block: %BB(\\d+)? Num States: (\\d+)");
+		std::regex e ("Basic Block: %(BB\\d+)? Num States: (\\d+)");
 		std::smatch group;
 		while(getline(origSchedule, line))
 		{
 			if(std::regex_search(line, group, e))
 			{
-				for (auto x:group) errs() << x << "\t";
-				errs() << "\n";
+				BBstates newBB;
+				std::string t = group[2];
+				newBB.stateCount = atoi(t.c_str());
+				newBB.stateRef	= group[1];
+				origBBStateNum.push_back(newBB);
 			}	
 		}
 		origSchedule.close();
 	}	
+	
+
+	void LostStateInjector::print()
+	{
+		for(BBState_iterator it=begin(), fin=end(); it != fin; ++it)
+		{
+			BBstates t = *it;
+			errs() << t.stateRef << "\t" << to_string(t.stateCount) << "\n"; 
+		} 
+	}
 
 } //namespace stchup
