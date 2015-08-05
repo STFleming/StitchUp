fba0620b861ced175920a7e81c33b4b1b4a12602
Shane Fleming <shane.fleming06@imperial.ac.uk>
2015-08-05 08:28:33 +0000

    Lost states are now injected into the Control FSM.

--- StitchUpBackend/LostStateInjector.h
+++ StitchUpBackend/LostStateInjector.h
@@ -25,41 +25,48 @@ namespace stchup {
 		void populateStitchupBBStateNum();
 		void incrementStitchupBBStateNum(BasicBlock *blk);
 		void locateLostStates();
+		void injectLostStates();
+		unsigned getLostStateCount(BasicBlock *blk);
+		void removeLostState(BasicBlock *blk);
+
+		
 		BBState_iterator orig_begin();
 		BBState_iterator orig_end();
 		BBState_iterator stitchup_begin();
 		BBState_iterator stitchup_end();
+		BBState_iterator lost_begin();
+		BBState_iterator lost_end();
+
+		//Debugging
 		void print();
 		 	
 	private:
 		std::vector<BBstates> origBBStateNum;
 		std::vector<BBstates> stitchupBBStateNum;
+		std::vector<BBstates> lostStates; //stateRef the BB we are refering to, stateCount = number of lost states
 		FiniteStateMachine *fsm;
 
  }; //namespace LostStateInjector
 
-	BBState_iterator LostStateInjector::orig_begin()
-	{	return origBBStateNum.begin();		}
-
-	BBState_iterator LostStateInjector::orig_end()
-	{	return origBBStateNum.end();		}
-
-	BBState_iterator LostStateInjector::stitchup_begin()
-	{	return stitchupBBStateNum.begin();		}
-
-	BBState_iterator LostStateInjector::stitchup_end()
-	{	return stitchupBBStateNum.end();		}
+	BBState_iterator LostStateInjector::orig_begin() { return origBBStateNum.begin(); }
+	BBState_iterator LostStateInjector::orig_end() { return origBBStateNum.end(); }
+	BBState_iterator LostStateInjector::stitchup_begin() { return stitchupBBStateNum.begin(); }
+	BBState_iterator LostStateInjector::stitchup_end() { return stitchupBBStateNum.end(); }
+	BBState_iterator LostStateInjector::lost_begin() { return lostStates.begin(); }
+	BBState_iterator LostStateInjector::lost_end() { return lostStates.end(); }
 
 	LostStateInjector::LostStateInjector(FiniteStateMachine *in)
 	{
 		fsm = in;
 		populateOrigBBStateNum();
 		populateStitchupBBStateNum();
+		locateLostStates();
+		injectLostStates();
+		errs() << "Lost State Injection Completed..\n";
 	}
 
 	LostStateInjector::~LostStateInjector()
 	{
-		locateLostStates();
 	}	
 
 	void LostStateInjector::populateOrigBBStateNum()
@@ -127,9 +134,74 @@ namespace stchup {
 				if(orig.stateRef == su.stateRef)
 				{
 					if(orig.stateCount != su.stateCount)
-						errs() << orig.stateRef << " has " << (orig.stateCount - su.stateCount)
-						<< " lost states.\n";
+					{
+						BBstates l;
+						l.stateRef = orig.stateRef;
+						l.stateCount = (orig.stateCount - su.stateCount);
+						lostStates.push_back(l);	
+					}
 				}
+			}
+		}
+	}
+
+	unsigned LostStateInjector::getLostStateCount(BasicBlock *blk)
+	{
+		if(blk==NULL) { return 0; }
+		for(BBState_iterator it=lost_begin(), fin=lost_end(); it != fin; ++it)
+		{
+			BBstates l = *it;
+			if(blk->getName() == l.stateRef)
+				return l.stateCount;
+		}		
+		return 0;
+	}
+
+	void LostStateInjector::removeLostState(BasicBlock *blk)
+	{
+		if(blk==NULL) { return; }
+		for(BBState_iterator it=lost_begin(), fin=lost_end(); it != fin; ++it)
+		{
+			BBstates l = *it;
+			if(blk->getName() == l.stateRef)
+			{
+				lostStates.erase(it);
+				return;
+			}
+		}		
+		return;
+	}
+
+	void LostStateInjector::injectLostStates()
+	{
+		for(FiniteStateMachine::StateListType::iterator it = fsm->begin(), fin=fsm->end(); it != fin; ++it)
+		{
+			State * s = it;
+			if(s->isTerminating())
+			{
+				BasicBlock *blk = s->getBasicBlock();
+				unsigned lostStateCount = getLostStateCount(blk);
+				if(lostStateCount > 0)
+				{
+					errs() << "Injecting " << to_string(lostStateCount) << " here\n";
+					State::Transition origTran = s->getTransition();
+					State::Transition newTran;
+					s->setTerminating(false);
+					s->setTransition(newTran);
+					for(unsigned ls=0; ls < lostStateCount; ls++)
+					{
+						std::string LostStateName = "LostState_" + blk->getName().str() 
+									    + "_" + to_string(ls);
+						State *t;
+						t = fsm->newState(s, LostStateName);
+						t->setBasicBlock(blk);
+						s->setDefaultTransition(t);
+						s = t;
+					}
+					s->setTransition(origTran);
+					s->setTerminating(true);
+					removeLostState(blk);
+				}	
 			}
 		}
 	}
