61df142d022cef3edf2b270e693a64579d68013f
STFleming <shane.fleming06@imperial.ac.uk>
2015-08-08 19:55:35 +0000

    Started work on wrapper both the stitchup and original modules are instantiated within a higher verilog module

--- libs/WrapperGeneration/generateWrapper.py
+++ libs/WrapperGeneration/generateWrapper.py
@@ -3,21 +3,60 @@
 import sys, getopt
 import re
 
-def instantiateTop(origString):
-    orig_inst = ''
-    stitchup_inst = ''
+def getSignals(origString):
     regex = re.compile(r'module top\(\n((?:\s*[A-z0-9]+,?)*)\n\);', re.MULTILINE)
     m = regex.search(origString)
     if m:
         portregex = re.compile(r'\s*([A-z0-9]+),?')
         p = portregex.search(m.group(1))
-        orig_inst = 'top top_inst(\n'
-        for i in re.findall(portregex, m.group(1)):
-            orig_inst += '\t.' + i + '(' + i + '_orig),\n'
-        orig_inst = orig_inst[:-2]
-        orig_inst += '\n);\n'
-        return orig_inst
+        return re.findall(portregex, m.group(1))
     return "error"
+
+#Check to see if an item already exists
+def inList(siglist, signame):
+        for s in siglist:
+                if signame == s[0]:
+                        return True
+        return False
+
+#Function that takes the list of ports for the module and returns if they are inputs or outputs
+def gatherIOLists(inputfile, lop):
+        inputvfile = open(inputfile, 'r')
+        vin = inputvfile.readlines()
+        inputvfile.close()
+
+        inputlist = [ ]
+        outputlist = [ ]
+
+        for p in lop:
+                r_i = "input\s+(\[(\d+):(\d+)\])?\s*" + re.escape(p) + "\s*;"
+                r_o = "output\s+(?:reg)?\s*(\[(\d+):(\d+)\])?\s*" + re.escape(p) + "\s*;"
+                for l in vin:
+                        item = [ ]
+                        f_i = re.search(r_i, l)
+                        f_o = re.search(r_o, l)
+                        if f_i:
+                                if not inList(inputlist, p) and not inList(outputlist, p):
+                                        item.append(p)
+                                        if f_i.group(1) is not None:
+                                                item.append(int(f_i.group(2)))
+                                                item.append(int(f_i.group(3)))
+                                        else:
+                                                item.append(0)
+                                                item.append(0)
+                                        inputlist.append(item)
+
+                        if f_o:
+                                if not inList(outputlist, p) and not inList(inputlist, p):
+                                        item.append(p)
+                                        if f_o.group(1) is not None:
+                                                item.append(int(f_o.group(2)))
+                                                item.append(int(f_o.group(3)))
+                                        else:
+                                                item.append(0)
+                                                item.append(0)
+                                        outputlist.append(item)
+        return (inputlist, outputlist)
 
 def main(argv):
     original = ''
@@ -40,13 +79,45 @@ def main(argv):
         if opt in ("-w", "--wrapper"):
             wrapper = arg
     outfile = open(wrapper, 'w')
-    stitchString = open(stitchup, 'r').read()
     origString = open(original, 'r').read()
 
     wrapperString = "//Stitchup toplevel wrapper for" + original + ", will flag an error on CFG faults.\n"
+    
+    signals = getSignals(origString)
 
-    print instantiateTop(origString) 
+    #Declare the port list of the topmost module
+    wrapperString += "\nmodule topmost(\n"
+    for s in signals:
+        wrapperString += '\t' + s +',\n'
+    wrapperString = wrapperString[:-2]
+    wrapperString += '\n);\n\n'
 
+    #Instantiate the signals that are used in the I/O top level ports
+    #For this we need to grab the direction
+    (inputlist, outputlist) = gatherIOLists(original, signals)
+
+    #Instantiate the input/output with the correct widths
+    for i in inputlist:
+        wrapperString += 'input [' + str(i[1]) + ':' + str(i[2]) +'] ' + i[0] + ';\n' 
+    for o in outputlist:
+        wrapperString += 'output wire [' + str(o[1]) + ':' + str(o[2]) + '] ' + o[0] + ';\n'
+
+    #Instantiate the original LegUp component
+    wrapperString += '\ntop top_inst(\n'
+    for s in signals: 
+        wrapperString += '\t.' + s + '(' + s + '_orig),\n'
+    wrapperString = wrapperString[:-2]
+    wrapperString += '\n);\n'
+
+    #Instantiate the StitchUp component
+    wrapperString += '\nstitchup_top stitchup_top_inst(\n'
+    for s in signals: 
+        wrapperString += '\t.' + s + '(stitchup_' + s + '),\n'
+    wrapperString = wrapperString[:-2]
+    wrapperString += '\n);\n'
+
+
+    wrapperString += 'endmodule\n'
     outfile.write(wrapperString)
     outfile.close()
 
