11539ff64794305f544b28d7ed4f1bd5f260b615
STFleming <sf306@ic.ac.uk>
2015-09-10 15:10:32 +0000

    system construction now includes the config chaos monkey for configuration error testing.

--- libs/AXIWrapper/vivado_build.tcl
+++ libs/AXIWrapper/vivado_build.tcl
@@ -11,8 +11,11 @@ set synth_language "VHDL"
 set sim_language "VHDL"
 set bd_name "zynq_sys"
 set stitchup_ip_dir "./AXIWrapperStitchUp_1.0/"
+set configMonkey "./sem_axi_1_1.0/"
 set full_ip_name "imperial:user:AXIWrapperStitchUp:1.0"
 set ip_instantiation_name "AXIWrapperStitchUp_0"
+set configMonkeyFullName "imperial:stitchup:sem_axi_1:1.0"
+set configMonkeyInstantName "sem_axi_1_0"
 
 create_project $temp_prj_name $current_dir/tmp/$temp_prj_name -part $fpga_part -force
 set_property board $board_name [current_project]
@@ -31,7 +34,7 @@ endgroup
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" }  [get_bd_cells processing_system7_0]
 
 #Add the output directory where the GOS IP core has been created
-set_property ip_repo_paths $stitchup_ip_dir [current_fileset]
+set_property ip_repo_paths ./ [current_fileset]
 update_ip_catalog
 
 #Add our newly generated IP block
@@ -39,9 +42,18 @@ startgroup
 create_bd_cell -type ip -vlnv $full_ip_name $ip_instantiation_name
 endgroup
 
+#Add our newly generated IP block
+startgroup
+create_bd_cell -type ip -vlnv $configMonkeyFullName $configMonkeyInstantName
+endgroup
+
 #Connect the IP core to the PS via AXI using the names defined in Component.xml in the generated IP core
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" }  [get_bd_intf_pins $ip_instantiation_name/S00_AXI]
 set_property offset 0x43C00000 [get_bd_addr_segs {processing_system7_0/Data/SEG_AXIWrapperStitchUp_0_S00_AXI_reg}]
+
+#Connect the IP core to the PS via AXI using the names defined in Component.xml in the generated IP core
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" }  [get_bd_intf_pins $configMonkeyInstantName/S00_AXI]
+set_property offset 0x42C00000 [get_bd_addr_segs {processing_system7_0/Data/SEG_sem_axi_1_0_S00_AXI_reg}]
 
 #Save the current board design
 save_bd_design
