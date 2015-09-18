a3cac0ddd4f1acff290c1b18d7cc8e1b6c0ca51a
STFleming <sf306@ic.ac.uk>
2015-09-18 14:44:16 +0000

    I saw a data errorgit add src/hlsKicker.c  Wooooo. removed tabbing for neater csv parsing.

--- zynq/seuInjection/sw_driver/src/hlsKicker.c
+++ zynq/seuInjection/sw_driver/src/hlsKicker.c
@@ -31,13 +31,13 @@ int main(int argc, char *argv[])
 
     //check for errors
     if(*((int*)(hlsdev)+4) != 0){
-        printf("%s, \t%d, \t%d, \t%d, \t%d, \t%d, \t%d, \t%d, \tERROR\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6));
+        printf("%s,%d,%d,%d,%d,%d,%d,%d,ERROR\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6));
         //printf("CFG Error detected\n");
         return 1;
     }
 
     //printf("Circuit completed successfully ans=%d\n", *((int *)(hlsdev)+3));
-    printf("%s, \t%d, \t%d, \t%d, \t%d, \t%d, \t%d, \t%d, \tOKAY\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6));
+    printf("%s,%d,%d,%d,%d,%d,%d,%d,OKAY\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6));
 	return 0;
 }
 
