ecc45922e9f007fce7938dabec6e5df4c576d9b0
STFleming <sf306@ic.ac.uk>
2015-10-20 10:23:42 +0000

    updated to include cycle count.

--- zynq/seuInjection/sw_driver/src/hlsKicker.c
+++ zynq/seuInjection/sw_driver/src/hlsKicker.c
@@ -26,18 +26,18 @@ int main(int argc, char *argv[])
     *((int*)(hlsdev)) = 0;
 
     //block till finish
-    while((*((int*)(hlsdev)+2) == 0) || *((int *)(hlsdev)+4) != 0){}
+    while((*((int*)(hlsdev)+2) == 0)){}
     //while(*((int*)(hlsdev)+3) != 46){}
 
     //check for errors
     if(*((int*)(hlsdev)+4) != 0){
-        printf("%s,%d,%d,%d,%d,%d,%d,%d,ERROR\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6));
+        printf("%s,%d,%d,%d,%d,%d,%d,%d,ERROR\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6), *((int*)(hlsdev)+7));
         //printf("CFG Error detected\n");
         return 1;
     }
 
     //printf("Circuit completed successfully ans=%d\n", *((int *)(hlsdev)+3));
-    printf("%s,%d,%d,%d,%d,%d,%d,%d,OKAY\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6));
+    printf("%s,%d,%d,%d,%d,%d,%d,%d,%d,OKAY\n", linearaddr, *((int*)(hlsdev)),*((int*)(hlsdev)+1),*((int*)(hlsdev)+2),*((int*)(hlsdev)+3),*((int*)(hlsdev)+4), *((int*)(hlsdev)+5), *((int*)(hlsdev)+6), *((int*)(hlsdev)+7));
 	return 0;
 }
 
