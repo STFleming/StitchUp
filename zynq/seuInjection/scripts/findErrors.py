38e4565d7e2bef1c882c5ff583313509699ea961
STFleming <sf306@ic.ac.uk>
2015-10-20 10:46:47 +0000

    now searches for mismatches in the number of cycles.

--- zynq/seuInjection/scripts/findErrors.py
+++ zynq/seuInjection/scripts/findErrors.py
@@ -5,20 +5,23 @@ import csv
 def main(argv):
     csvresfilename = ''
     expectedval = ''
+    cycles = ''
     try:
-            opts, args = getopt.getopt(argv, "hi:e", ["input=", "expected="])
+            opts, args = getopt.getopt(argv, "hi:e:c", ["input=", "expected=", "cycles="])
     except getopt.GetoptError:
-            print 'Usage: python findErrors.py --input <results>.csv --expected <expected ans value>'
+            print 'Usage: python findErrors.py --input <results>.csv --expected <expected ans value> --cycles <expected number of cycles>'
             sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'Usage: python findErrors.py --input <results>.csv --expected <expected ans value>'
+            print 'Usage: python findErrors.py --input <results>.csv --expected <expected ans value> --cycles <expected number of cycles>'
             sys.exit()
         if opt in ("-i", "--input"):
             csvresfilename = arg
         if opt in ("-e", "--expected"):
             expectedval = arg
+        if opt in ("-c", "--cycles"):
+            cycles = arg
     assert(csvresfilename != '')
 
     DE_T_CE_T = 0;
@@ -26,9 +29,11 @@ def main(argv):
     DE_T_CE_F = 0;
     DE_F_CE_F = 0;
     TIMEOUTS = 0;
+    CYCLES = 0;
     dE = False
     cE = False
     tO = False
+    timeE = False
 
     with open(csvresfilename, 'rb') as csvfile:
 	csvValues = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -39,6 +44,8 @@ def main(argv):
 			tO = True
 		if row[4] != str(expectedval):
 			dE = True
+		if row[8] != str(cycles):
+			timeE = True
 		if dE or cE:
 			#Frame, bit, dE, cE
 			Frame = int(row[0]) >> 12 
@@ -57,14 +64,18 @@ def main(argv):
 				DE_F_CE_F = DE_F_CE_F + 1;
 		if tO:
 			TIMEOUTS = TIMEOUTS + 1
+		if timeE:
+			CYCLES = CYCLES + 1
 		dE = False
 		cE = False
 		tO = False
+		timeE = False
 
 	print "|Total Faults: " + str(DE_T_CE_T + DE_T_CE_F + DE_F_CE_T)
 	print "|Data Only Faults: " + str(DE_T_CE_F)
 	print "|StitchUp Detected Faults: " + str(DE_T_CE_T + DE_F_CE_T)
 	print "|Total Timeouts: " + str(TIMEOUTS)
+	print "|Total Cycle Errors: " + str(CYCLES)
 	print "|Essential bits that are not critical: " + str(DE_F_CE_F)
 
 if __name__ == "__main__":
